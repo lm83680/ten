8dafe600ae9db3ca87768ad2373922a435594ebb
qcter <qcter@icloud.com>
2024-08-23 19:40:50 +0800

    init

---
+++ lib/src/button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TenButton extends StatelessWidget {
+  const TenButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
